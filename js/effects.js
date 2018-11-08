d1b9c7243de82ab986a2a3658975da3c5da756a0
Szabolcs Damján <damjan.szabolcs@gmail.com>
2018-11-08 11:15:49 +0100

    func ok

--- js/effects.js
+++ js/effects.js
@@ -2,27 +2,36 @@ class Effects {
 
     constructor(store) {
         this.subscriptions = [
-            { scope: 'messages', effect: this.renderMsg }
+            { scope: 'messages', effect: this.renderMsg },
+            { scope: 'messages', effect: this.sendMsg }
         ];
 
         this.subscriptions.forEach((sub) => {
             store.subscribe(sub.scope, sub.effect.bind(this))
         })
 
-        var socket = io.connect('http://185.13.90.140:8081/');
-        socket.on('connect', function (e) {
+        this.socket = io.connect('http://185.13.90.140:8081/');
+        this.socket.on('connect', function (e) {
                 console.log(e)
          });
-        socket.on('message', function (data) {
-            console.log(data)
+         this.socket.on('message', function (data) {
+            console.log(data);
+            app.store.action('receive', data );
          });
-        socket.on('disconnect', function () {
+         this.socket.on('disconnect', function () {
             console.log(e)
         });
     }
 
     renderMsg(data) {
         document.querySelector('#feed').insertAdjacentHTML('beforeend', appTemplates.message(data[data.length - 1]))
+    }
+
+    sendMsg(data){
+        let msg = data[data.length - 1];
+        if(msg.type == 'local') {
+            this.socket.emit('message',{message: data.message, user: data.user })
+        }
     }
 
 }
